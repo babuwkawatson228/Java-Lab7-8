a9cb38ea0d54e1dfd2d532d24e33a4949a6bcad1
babuwkawatson228 <155854933+babuwkawatson228@users.noreply.github.com>
2025-11-27 04:11:30 +0100

    Update Blowfish.java

---
+++ Blowfish.java
@@ -0,0 +1,49 @@
+public class Blowfish extends Fish implements AnimalMove, AnimalBehaviour{
+    private String fishType;
+
+    Blowfish(){
+        super();
+        fishType = "NoType";
+    }
+    public Blowfish(String name, int age, int weight, String scaleColor, String fishType){
+        super(name, age, weight, scaleColor);
+        this.fishType = fishType;
+    }
+    public Blowfish(int age, String scaleColor, String fishType) {
+        super(age, scaleColor);
+        this.fishType = fishType;
+    }
+
+    public void inflate() {
+        System.out.println("Blowfish is inflating");
+    }
+
+    @Override
+    public void eat(String foodName) {
+        System.out.println("Blowfish is eating " + foodName);
+    }
+
+    @Override
+    public void getVoice() {
+        System.out.println("Blowfish makes sound");
+    }
+
+    public String getFishType() { return fishType; }
+    public void setFishType(String fishType) { this.fishType = fishType; }
+
+    @Override
+    public String toString(){
+        return "Blowfish{"+
+                "fishType='" + fishType + '\'' +
+                '}';
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Blowfish is sleeping");
+    }
+    @Override
+    public void move(Object obj) {
+        System.out.println("Blowfish is moving");
+    }
+}
